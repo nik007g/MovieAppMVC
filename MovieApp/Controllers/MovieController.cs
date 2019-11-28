bea375d83d1e0d196eef1bf859df153cfa48d399
Nikhil Gundale <nikhil.gundale@fusionstak.com>
2019-11-28 19:12:48 +0530

    Updated Using Interface

--- MovieApp/Controllers/MovieController.cs
+++ MovieApp/Controllers/MovieController.cs
@@ -17,7 +17,6 @@ namespace MovieApp.Controllers
         {
             objmovie = new MovieDataAcessLayerEF(context);
         }
-
         [HttpGet]
         public IActionResult Create()
         {
